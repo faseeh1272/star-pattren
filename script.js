88c22bab8895f3d1f0e7832b7360c0d9ee855d54
faseeh1272 <syedfaseehnaqvi2@gmail.com>
2024-10-11 12:23:51 -0700

    some changes

--- script.js
+++ script.js
@@ -1,103 +1,155 @@
-var userValue = prompt("Enter a first number for triangle")
+var userValue =prompt("Enter A first Number for triangal")
 
-for (var i = 1; i <= 5; i++) {
-
-    for (var j = 5; j > i; j--) {
-
-
-        document.write("&nbsp;");
+for(i=1; i<=5 ; i++)
+{
+    for (j=5; j>i; j--)
+    {
+        document.write(" &nbsp; ");
     }
-    for (var k = 1; k <= ((2 * i) - 1); k++) {
-
+    for(k=1 ; k<=((2*i)-1); k++)
+    {
         document.write("*");
     }
-
     document.write("<br>");
-
-
 }
 
+
+document.write("<br>");
+
+
+// triangal pattren End
+
+
+
+// Reversed triangal pattaren start
+var userValue=prompt("Enter a second number")
+for(i=5; i>=1; i--)
+{
+    for(j=5;j>i; j--)
+        {
+            document.write(" &nbsp ");
+        }
+        for (k=1; k<=((2*i)-1);k++)
+        {
+            document.write("*");
+        }
+    document.write("<br>");
+}
+
+
+
+// Reversed triangal pattaren End
+
+
+
+// Diamond pattren is Start
 document.write("<br>")
 
-// reverse triangle pattren start
 
-var userValue = prompt("Enter a second value")
-
-for (var i = 1; i <= 5; i--) {
-
-    for (var j = 5; j > i; j--) {
-        document.write(" &nbsp; ");
-    }
-    for (var k = 1; k <= ((2 * i) - 1); k++) {
-        document.write("*");
-
-    }
+var userValue=prompt("Enter a third number")
 
 
-    document.write("<br>")
-
-}
-
-// 3 : diamond pattren start :
-var userValue = prompt("Enter a third value")
-for (var i = 1; i<=7; i++){
-
-
-    for(var j = 7; j>i; j--){
-
-        document.write("&nbsp;");
-    }
-    for (var k = 1; k<=((2*i)-1);  k++){
-        document.write("*");
+for(i=1; i<=7 ; i++)
+    {
+        for (j=7; j>i; j--)
+        {
+            document.write(" &nbsp; ");
+        }
+        for(k=1 ; k<=((2*i)-1); k++)
+        {
+            document.write("*");
+        }
+        document.write("<br>");
     }
 
-    document.write("<br>");
-}
+    for(i=7; i>=1; i--)
+        {
+            for(j=7;j>i; j--)
+                {
+                    document.write(" &nbsp ");
+                }
+                for (k=1; k<=((2*i)-1);k++)
+                {
+                    document.write("*");
+                }
+            document.write("<br>");
+        }
 
-for (var i = 7; i>=1; i--){
+
+        document.write("<br>")
+
+        // / Diamond pattren is End
 
 
-    for(var j = 7; j>i; j--){
-        document.write(" &nbsp; ");
+
+        
+ var userValue=prompt("enter afour num")
+
+
+ for(i=5; i>=1; i--)
+    {
+        for(j=5;j>i; j--)
+            {
+                document.write(" &nbsp ");
+            }
+            for (k=1; k<=((2*i)-1);k++)
+            {
+                document.write("*");
+            }
+        document.write("<br>");
     }
-    for(var k = 1; k>=((2*i)-1);   k++){
-        document.write("*");
 
-    }
-    document.write("<br>")
-}
-
-var userValue=prompt("enter afour num")
-
-
-for(i=5; i>=1; i--)
-   {
-       for(j=5;j>i; j--)
-           {
-               document.write(" &nbsp ");
-           }
-           for (k=1; k<=((2*i)-1);k++)
-           {
-               document.write("*");
-           }
-       document.write("<br>");
-   }
-
-   
+    
 for(i=1; i<=5 ; i++)
-   {
-       for (j=5; j>i; j--)
-       {
-           document.write(" &nbsp; ");
-       }
-       for(k=1 ; k<=((2*i)-1); k++)
-       {
-           document.write("*");
-       }
-       document.write("<br>");
-   }
+    {
+        for (j=5; j>i; j--)
+        {
+            document.write(" &nbsp; ");
+        }
+        for(k=1 ; k<=((2*i)-1); k++)
+        {
+            document.write("*");
+        }
+        document.write("<br>");
+    }
 
 
 document.write("<br>")  
 
 
+
+
+// for(let i = 1; i<10; i = i+2){
+    
+//      for(let k = 9; k>=i; k--){
+//          document.write("&nbsp;") 
+//      }
+
+//     for(let j = 1; j<=i; j++){
+//         document.write("*") 
+       
+//     }
+//     document.write("<br>") 
+//  }
+
+// var  userValue =prompt("enter a num")
+// main()
+// {
+
+//     let i,j,k;
+//     for(let i=1;i<=5;i++)
+//     {
+//         for(let j=5;j>i;j--)
+//         {
+//            document.write(" ") // printf(" ");
+//         }
+//         for(let k=1;k<=i;k++)
+//         {
+//            document.write("* ") // printf(" * ")
+//         }
+//         printf("\n")
+//     }
+// }
+ 
+
+   
